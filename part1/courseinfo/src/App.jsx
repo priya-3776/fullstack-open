e1e95cbee5d5b6662a8292e49764a67f5a3a8ea0
priya-3776 <priyaaiml2025@gmail.com>
2025-09-28 22:27:34 +0530

    Part1 - Exercise 1.14

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -26,12 +26,20 @@ const App = () => {
     setVotes(copy)
   }
 
+  const maxVotes = Math.max(...votes)
+  const bestAnecdoteIndex = votes.indexOf(maxVotes)
+
   return (
     <div>
+      <h2>Anecdote of the day</h2>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={voteAnecdote}>vote</button>
       <button onClick={randomAnecdote}>next anecdote</button>
+
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[bestAnecdoteIndex]}</p>
+      <p>has {maxVotes} votes</p>
     </div>
   )
 }
