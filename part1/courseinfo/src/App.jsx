a92922988a5e1ef5dcef6fcaf1d9c44d19c6544e
Priya Dharshini <priyaaiml2025@gmail.com>
2025-09-28 19:57:13 +0530

    Part1 - Courseinfo exercises 1.3 to 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,8 +1,19 @@
 const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ]
+  }
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <p>Hello World, this is my first React app!</p>
+      <h1>{course.name}</h1>
+      <p>{course.parts[0].name} {course.parts[0].exercises}</p>
+      <p>{course.parts[1].name} {course.parts[1].exercises}</p>
+      <p>{course.parts[2].name} {course.parts[2].exercises}</p>
     </div>
   )
 }
