1ca44565d045db625c6dba8f378256e40bd52cd9
priya-3776 <priyaaiml2025@gmail.com>
2025-09-28 22:19:46 +0530

    Part1 - Exercise 1.7

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,6 +5,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const average = (good - bad) / (all || 1)
+  const positive = (good / (all || 1)) * 100
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -16,6 +20,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
