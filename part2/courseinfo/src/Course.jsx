8472af972cba238c43aa987a583f2a8642f1468b
priya-3776 <priyaaiml2025@gmail.com>
2025-09-29 19:29:07 +0530

    Exercise 2.5: Move Course and subcomponents to separate module

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -13,6 +13,7 @@ const Content = ({ parts }) => (
   </div>
 )
 
+
 // Main Course component
 const Course = ({ course }) => {
   const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
